b8befb1d1b2a7537ed3eb145fb1c13a1cd8bbb83
PIEPER <DAVID.PIEPER@dsisd.local>
2019-10-28 10:57:07 -0500

    Updated files to improve maven functionality Updated some files to be able to hopefully get Travis CI to be able to accept this as a proper maven build.

--- src/src/TrajectoryCalculatorRunner.java
+++ src/src/TrajectoryCalculatorRunner.java
@@ -1,9 +1,18 @@
 package src;
 
+import java.util.Scanner;
+
 public class TrajectoryCalculatorRunner 
 {
     public static void main(String args[]) 
     {
         System.out.println("Hello World");
+        Scanner reader = new Scanner(System.in);
+        System.out.print("What's your name?             ");
+        String nameAnswer = reader.nextLine();
+        System.out.print("What's your favorite color?   ");
+        String colorAnswer = reader.nextLine();
+        System.out.println("Hello, " + nameAnswer + ", your favorite color is " + colorAnswer);
+        reader.close();   
     }
 }
