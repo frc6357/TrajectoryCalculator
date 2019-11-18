4191913506cc3bc444dfd8651299b1abad186cda
mohamfurqan <furqanmehboob123@gmail.com>
2019-11-18 11:12:09 -0600

    Updated the classes to be able to write the code The code has been updated to be able to update and make the code work in a much more effective way.

--- src/main/java/TrajectoryCalculator/App.java
+++ src/main/java/TrajectoryCalculator/App.java
@@ -1,7 +1,7 @@
 package TrajectoryCalculator;
 
 import org.ejml.data.CMatrixRMaj;
-
+import java.util.*; 
 /**
  * Hello world!
  */
@@ -10,31 +10,6 @@ public final class App {
     {
 
     }
-    // private void CreatePointMatrix(int x1, int y1, int x2, int y2)
-    // {
-    //    double[][] matrix1;
-    //     matrix1 = new double[][] {{Math.pow(x1,3), Math.pow(x1,2), x1, 1}, 
-    //                             {Math.pow(x2,3), Math.pow(x2,2), x2, 1}, 
-    //                             {3*Math.pow(x1,2),2*x1, 1, 0},
-    //                             {3*Math.pow(x2,2), 2*x2, 1, 0}};
-    //}
-    // public void CMatrixRMaj1(float[][] data)
-    // {
-    //          data = new float[][] {{Math.pow(x1,3), Math.pow(x1,2), x1, 1}, 
-    //                                 {Math.pow(x2,3), Math.pow(x2,2), x2, 1}, 
-    //                                 {3*Math.pow(x1,2),2*x1, 1, 0},
-    //                                 {3*Math.pow(x2,2), 2*x2, 1, 0}};       
- 
-    // }
-    private String CreateInverseMatrix(int y1, int y2, int dy1, int dy2)
-    {
-        //CMatrixRMaj(float[4][4]);
-        //invert(LinearSolverDense<CMatrixRMaj> solver, CMatrixRMaj A, CMatrixRMaj A_inv);        
-        
-        return " ";
-        
-    }
-
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
